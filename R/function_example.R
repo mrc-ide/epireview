b6d16e094d37248f5b04e35a0e0551597ec37f9a
cm401 <christian.morgenstern@gmail.com>
2023-06-19 14:18:41 +0100

    delect function

--- R/function_example.R
+++
@@ -1,8 +0,0 @@
-#' Description of what function does
-#' 
-#' @param param1_name param1_description
-#' @param param2_name param2_description etc
-#' @return description of what is returned
-#' @examples 
-#' provide example
-#' @export (the export tag just allows the function to be exported)
