5d0f28dff3ec04ace7a68dbed58bd6a39ecf8b1e
rebeccanash <r.nash@imperial.ac.uk>
2023-06-19 19:07:14 +0100

    code cleaning and checks added

--- R/load_review_data.R
+++ R/load_review_data.R
@@ -1,14 +1,23 @@
-#' load_data function
+#' Load data for a particular pathogen
 #'
-#' @param table_type which type of table (parameter, outbreak, etc) should be loaded
+#' @param table_type which type of table (either article, parameter, outbreak,
+#' model) should be loaded
 #' @param pathogen name of pathogen
-#' @return return tibble with data for table_type and pathogen
+#' @return return data for table_type and pathogen
 #' @examples
-#' load_data("outbreak","margburg")
-#' @export (the export tag just allows the function to be exported)
-load_data <- function(table_type="parameter",pathogen="marburg")
+#' load_data("outbreak","marburg")
+#' @export
+load_data <- function(table_type = NA, pathogen = NA)
 {
-  data_tbl <- read.csv(paste0('data/',pathogen,'/',table_type,'_final.csv'))
+  if(is.na(table_type)){
+    stop("table_type must be one of either article, parameter, outbreak or model")
+  }
+
+  if(is.na(pathogen)){
+    stop("pathogen name must be supplied")
+  }
+
+  data_tbl <- read.csv(paste0('data/', pathogen, '_', table_type, '.csv'))
   return(data_tbl)
 }
 
