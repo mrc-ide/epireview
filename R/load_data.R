dd0c704f3146e7c5534b306bdbc14b3c672e3fda
rebeccanash <r.nash@imperial.ac.uk>
2023-06-20 14:19:13 +0100

    modify load_data check and add importfrom

--- R/load_data.R
+++ R/load_data.R
@@ -4,22 +4,20 @@
 #' model) should be loaded
 #' @param pathogen name of pathogen
 #' @return return data for table_type and pathogen
+#' @importFrom readr read_csv
 #' @examples
 #' load_data("outbreak","marburg")
 #' @export
 load_data <- function(table_type = NA, pathogen = NA)
 {
-  if(is.na(table_type)){
-    stop("table_type must be one of either article, parameter, outbreak or model")
+  if(is.na(table_type) || is.na(pathogen)){
+    stop("table_type and pathogen name must be supplied. table_type can be
+         one of either 'article', 'parameter', 'outbreak' or 'model'")
   }
 
-  if(is.na(pathogen)){
-    stop("pathogen name must be supplied")
-  }
-
-  data_tbl <- readr::read_csv(system.file("data",
-                                          paste0(pathogen, "_", table_type, ".csv"),
-                                          package = "epireview"))
+  data_tbl <- read_csv(system.file("data",
+                                   paste0(pathogen, "_", table_type, ".csv"),
+                                   package = "epireview"))
   return(data_tbl)
 }
 
