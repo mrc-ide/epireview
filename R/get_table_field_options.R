2c9ee8652285017a8e1181b6282e0f7afc0c71e5
cm401 <christian.morgenstern@gmail.com>
2023-06-24 15:02:10 +0100

    initial revision

---
+++ R/get_table_field_options.R
@@ -0,0 +1,23 @@
+#' Utility to obtain available table options to pick from
+#'
+#' @param table_type which type of table (either article, parameter, outbreak,
+#' model) should be loaded
+#' @param field
+#' @return return data for table_type and pathogen
+#' @importFrom readr read_csv
+#' @examples
+#' get_available_table_field_options("model",field='Model type')
+#' @export
+get_available_table_field_options <- function(table_type=NA,field="all")
+{
+  file_path_ob  <- system.file("data", paste0("access_db_dropdown_", table_type,"s.csv"), package = "epireview")
+  if(file_path_ob=="") file_path_ob <- paste0("data/access_db_dropdown_", table_type,"s.csv")
+  model_options <- read_csv(file_path_ob)
+
+  if(field=="all")
+  {
+    return(model_options %>% mutate(across(everything(),~replace_na(.x, ""))))
+  } else {
+    return(as.vector(na.omit(model_options[[field]])))
+  }
+}
