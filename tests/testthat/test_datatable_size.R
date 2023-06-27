e2e2e4a881c5d2ab9e235912b856c94c9ffb97a5
cm401 <christian.morgenstern@gmail.com>
2023-06-27 16:17:37 +0100

    initial revision

---
+++ tests/testthat/test_datatable_size.R
@@ -0,0 +1,5 @@
+# Check that data exists and has minimum size
+
+marburg_article_data <- load_data('article','marburg')
+
+test_that("Marburg articles have at least 58 lines",{expect_gte(dim(marburg_article_data)[1],58)})
