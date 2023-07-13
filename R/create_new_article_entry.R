74d476ed6e79e0b4c12e0dcbd0022967a741aa4c
rebeccanash <r.nash@imperial.ac.uk>
2023-07-13 16:35:02 +0100

    edits for append_new_entry_to_table and create_new_article_entry

--- R/create_new_article_entry.R
+++ R/create_new_article_entry.R
@@ -2,13 +2,12 @@
 #'
 #' @param pathogen name of pathogen
 #' @param new_article all the required details for the new article
-#' @param vignette_prepend string to allowing loading data from vignettes
-#' @return return data for new row to be added with append_new_entry_to_table function
-#' @importFrom tibble as_tibble
-#' @importFrom tibble as_tibble_row
-#' @importFrom validate validator
-#' @importFrom validate confront
-#' @importFrom validate summary
+#' @param vignette_prepend string to allow loading data in vignettes
+#' @return return new row of data to be added to the article data set using the
+#' append_new_entry_to_table() function
+#' @importFrom tibble as_tibble as_tibble_row
+#' @importFrom validate validator confront summary
+#' @importFrom dplyr rowwise mutate select filter
 #' @examples
 #' create_new_article_entry(pathogen = "marburg", new_article = c( list( "first_author_first_name" = as.character("Joe")),
 #'                                                     list( "first_author_surname"    = as.character("Blocks")),
@@ -54,21 +53,33 @@ create_new_article_entry <- function(pathogen = NA,
                                      vignette_prepend = "")
 {
   #read current article data for pathogen
-  old_articles         <- as_tibble(load_data(table_type = 'article',pathogen = pathogen, vignette_prepend = vignette_prepend))
-  new_row              <- as_tibble_row(new_article)
+  old_articles <- as_tibble(load_data(table_type = 'article',
+                                      pathogen = pathogen,
+                                      vignette_prepend = vignette_prepend))
 
-  # generate the below quanties
-  new_row$article_id   <- max(old_articles$article_id) + 1             #in access db the article ids need to skip forward appropriately
-  new_row$covidence_id <- if(max(old_articles$covidence_id) > 1000000) max(old_articles$covidence_id) + 1 else max(old_articles$covidence_id) + 1000000  #this is to ensure we don't infer with natural covid_ids
-  new_row$pathogen     <- switch(pathogen,"marburg"="Marburg virus",
-                                 "ebola"="Ebola virus", NA)      #need to complete list for all pathogens
+  new_row <- as_tibble_row(new_article)
+
+  # generate the below quantities
+  new_row$article_id <- max(old_articles$article_id) + 1
+  new_row$covidence_id <- if(max(old_articles$covidence_id) > 1000000){
+    max(old_articles$covidence_id) + 1
+    } else {
+      max(old_articles$covidence_id) + 1000000
+    }
+
+  # update this as new pathogens are added
+  new_row$pathogen <- switch(pathogen,
+                             "marburg" = "Marburg virus",
+                             "ebola" = "Ebola virus", NA)
   new_row$double_extracted <- FALSE
 
-  new_row               <- new_row %>% dplyr::rowwise() %>% mutate(score = mean(c(qa_m1,qa_m2,qa_a3,qa_a4,qa_d5,qa_d6,qa_d7),na.rm=TRUE)) %>%
-    dplyr::select(colnames(old_articles))
+  new_row <- new_row %>% rowwise() %>%
+    mutate(score = mean(c(qa_m1, qa_m2, qa_a3, qa_a4, qa_d5, qa_d6, qa_d7),
+                        na.rm = TRUE)) %>%
+    select(colnames(old_articles))
 
-  sprintf("%s",colnames(old_articles))
-  sprintf("%s",colnames(new_row))
+  sprintf("%s", colnames(old_articles))
+  sprintf("%s", colnames(new_row))
 
   # check that article doesn't exist already in data by looking for doi (if it exists)
   if(is.character(new_row$doi) & new_row$doi %in% na.omit(old_articles$doi))
@@ -83,28 +94,24 @@ create_new_article_entry <- function(pathogen = NA,
     doi_is_character                     = is.character(doi),
     transmission_route_is_character      = is.character(transmission_route),
     assumptions_is_character             = is.character(assumptions),
-    code_available_check                 = code_available %in% c(0,1,NA),
+    code_available_check                 = code_available %in% c(0, 1, NA),
     outbreak_date_year_is_integer        = is.integer(outbreak_date_year),
     outbreak_date_year_after_1800        = outbreak_date_year > 1800,
-    outbreak_date_year_not_future        = outbreak_date_year < (as.integer(substring(Sys.Date(),1,4))+2),
-    qa_m1                                = qa_m1 %in% c(0,1,NA),
-    qa_m2                                = qa_m2 %in% c(0,1,NA),
-    qa_a3                                = qa_a3 %in% c(0,1,NA),
-    qa_a4                                = qa_a4 %in% c(0,1,NA),
-    qa_d5                                = qa_d5 %in% c(0,1,NA),
-    qa_d6                                = qa_d6 %in% c(0,1,NA),
-    qa_d7                                = qa_d7 %in% c(0,1,NA)
+    outbreak_date_year_not_future        = outbreak_date_year < (as.integer(substring(Sys.Date(), 1, 4)) + 2),
+    qa_m1                                = qa_m1 %in% c(0, 1, NA),
+    qa_m2                                = qa_m2 %in% c(0, 1, NA),
+    qa_a3                                = qa_a3 %in% c(0, 1, NA),
+    qa_a4                                = qa_a4 %in% c(0, 1, NA),
+    qa_d5                                = qa_d5 %in% c(0, 1, NA),
+    qa_d6                                = qa_d6 %in% c(0, 1, NA),
+    qa_d7                                = qa_d7 %in% c(0, 1, NA)
   )
 
   rules_output  <- confront(new_row, rules)
-  rules_summary <- validate::summary(rules_output)
+  rules_summary <- summary(rules_output)
 
-  if(sum(rules_summary$fails)>0)
-    stop(as_tibble(rules_summary) %>% filter(fails>0) )
+  if(sum(rules_summary$fails) > 0)
+    stop(as_tibble(rules_summary) %>% filter(fails > 0))
 
   return(new_row)
 }
-
-
-
-
