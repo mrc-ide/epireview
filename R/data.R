47b9332ce018820bab06baf61edd9f1724d4609e
rebeccanash <r.nash@imperial.ac.uk>
2023-07-13 12:54:03 +0100

    start dataset documentation

---
+++ R/data.R
@@ -0,0 +1,65 @@
+#' Data on the models identified in the systematic review of Marburg articles
+#'
+#' @description
+#'
+#' This data set gives:
+#'
+#' XXYY
+#'
+#' @name marburg_model.csv
+#' @docType data
+#' @format A csv file containing the following parameters:
+#'
+#' XXYY
+#'
+#' @source Cuomo-Dannenburg G, McCain K, McCabe R, Unwin HJT, Doohan P, Nash RK,
+#' et al. Marburg Virus Disease outbreaks, mathematical models, and disease
+#' parameters: a Systematic Review [Internet]. medRxiv; 2023 [cited 2023 Jul 13].
+#' p. 2023.07.10.23292424.
+#' Available from: https://www.medrxiv.org/content/10.1101/2023.07.10.23292424v1
+#'
+NULL
+
+#' Data on the outbreaks identified in the systematic review of Marburg articles
+#'
+#' @description
+#'
+#' This data set gives:
+#'
+#' XXYY
+#'
+#' @name marburg_outbreak.csv
+#' @docType data
+#' @format A csv file containing the following parameters:
+#'
+#' XXYY
+#'
+#' @source Cuomo-Dannenburg G, McCain K, McCabe R, Unwin HJT, Doohan P, Nash RK,
+#' et al. Marburg Virus Disease outbreaks, mathematical models, and disease
+#' parameters: a Systematic Review [Internet]. medRxiv; 2023 [cited 2023 Jul 13].
+#' p. 2023.07.10.23292424.
+#' Available from: https://www.medrxiv.org/content/10.1101/2023.07.10.23292424v1
+#'
+NULL
+
+#' Data on the parameters identified in the systematic review of Marburg articles
+#'
+#' @description
+#'
+#' This data set gives:
+#'
+#' XXYY
+#'
+#' @name marburg_parameter.csv
+#' @docType data
+#' @format A csv file containing the following parameters:
+#'
+#' XXYY
+#'
+#' @source Cuomo-Dannenburg G, McCain K, McCabe R, Unwin HJT, Doohan P, Nash RK,
+#' et al. Marburg Virus Disease outbreaks, mathematical models, and disease
+#' parameters: a Systematic Review [Internet]. medRxiv; 2023 [cited 2023 Jul 13].
+#' p. 2023.07.10.23292424.
+#' Available from: https://www.medrxiv.org/content/10.1101/2023.07.10.23292424v1
+#'
+NULL
