7c26c79eee6004761380af662e9c25e670fc0c48
rebeccanash <r.nash@imperial.ac.uk>
2023-07-14 17:00:34 +0100

    minor lint edits and documentation return edit

--- R/append_new_entry_to_table.R
+++ R/append_new_entry_to_table.R
@@ -14,7 +14,8 @@
 #' @param write_table write appended table (either TRUE or FALSE). If TRUE the
 #' new table will be written as a csv file in the data folder of a locally
 #' cloned repository.
-#' @return Null (updates relevant table in a locally cloned repository)
+#' @return table with new entry (if write_table = TRUE the relevant table in a
+#' locally cloned repository is updated)
 #'
 #' @export
 append_new_entry_to_table <- function(pathogen = NA,
@@ -22,38 +23,43 @@ append_new_entry_to_table <- function(pathogen = NA,
                                       new_row = NA,
                                       validate = TRUE,
                                       write_table = FALSE,
-                                      vignette_prepend = "")
-{
+                                      vignette_prepend = "") {
+
   old_table <- load_data(table_type = table_type,
                          pathogen = pathogen,
                          vignette_prepend = vignette_prepend)
 
-  if(!((sum(colnames(new_row) == colnames(old_table)) /
+  if (!((sum(colnames(new_row) == colnames(old_table)) /
         length(colnames(new_row))) == 1))
-    stop('Schemas of new row and data sets do not match')
+    stop("Schemas of new row and data sets do not match")
 
-  if(table_type=='article' & new_row$article_id %in% old_table$article_id)
-    stop('article_id already exists in the data set')
+  if (table_type == "article" & new_row$article_id %in% old_table$article_id)
+    stop("article_id already exists in the data set")
 
   new_table <- rbind(old_table, new_row)
 
-  if(validate)
-  {
-    if(table_type=='article')
-      create_new_article_entry(pathogen = pathogen, new_article = new_row %>%
-                                 as.vector(), vignette_prepend = vignette_prepend)
-    if(table_type=='outbreak')
-      create_new_outbreak_entry(pathogen = pathogen, new_outbreak = new_row %>%
-                                  as.vector(), vignette_prepend = vignette_prepend)
-    if(table_type=='model')
-      create_new_model_entry(pathogen = pathogen, new_model = new_row %>%
-                               as.vector(), vignette_prepend = vignette_prepend)
-    if(table_type=='parameter')
-      create_new_parameter_entry(pathogen = pathogen, new_param = new_row %>%
-                                   as.vector(), vignette_prepend = vignette_prepend)
+  if (validate) {
+    if(table_type == "article")
+      create_new_article_entry(pathogen = pathogen,
+                               new_article = new_row %>% as.vector(),
+                               vignette_prepend = vignette_prepend)
+    if(table_type == "outbreak")
+      create_new_outbreak_entry(pathogen = pathogen,
+                                new_outbreak = new_row %>% as.vector(),
+                                vignette_prepend = vignette_prepend)
+    if(table_type == "model")
+      create_new_model_entry(pathogen = pathogen,
+                             new_model = new_row %>% as.vector(),
+                             vignette_prepend = vignette_prepend)
+    if(table_type == "parameter")
+      create_new_parameter_entry(pathogen = pathogen,
+                                 new_param = new_row %>% as.vector(),
+                                 vignette_prepend = vignette_prepend)
   }
 
-  if(write_table) write.csv(new_table,paste0(vignette_prepend, 'data/', pathogen, '_', table_type, '.csv'))
+  if(write_table)
+    write.csv(new_table, paste0(vignette_prepend, "data/", pathogen, "_",
+                                table_type, ".csv"))
 
   return(new_table)
 }
