4d5b951f20d236180cec02e05d3dd2b60ae9fc00
rebeccanash <r.nash@imperial.ac.uk>
2023-07-17 18:46:19 +0100

    clean and document remaining functions

--- R/forest_plot_r.R
+++ R/forest_plot_r.R
@@ -1,60 +1,66 @@
 #' Create forest plot for reproduction numbers
 #'
-#' @param df data with outbreak information
-#' @return returns flextable
+#' @param df processed data with parameter information (see vignette for each
+#' pathogen)
+#' @return returns plot with a summary of reproduction number estimates
+#' @importFrom dplyr filter arrange mutate group_by
+#' @importFrom ggplot2 aes theme_bw geom_point scale_y_discrete
+#' scale_x_continuous geom_segment geom_errorbar labs scale_color_brewer
+#' scale_shape_manual theme guides element_text guide_legend
 #' @examples
-#'
+#' forest_plot_R(df = data)
 #' @export
-forest_plot_R <- function(df){
+forest_plot_R <- function(df) {
 
   parameter <- "Reproduction number"
 
   df_R <- df %>% filter(parameter_class == parameter) %>%
     mutate(parameter_value = as.numeric(parameter_value)) %>%
     group_by(parameter_type) %>%
-    dplyr::arrange(first_author_surname)
+    arrange(first_author_surname)
 
   df_plot <- df_R %>% filter(parameter_class == parameter) %>%
     mutate(parameter_value = as.numeric(parameter_value)) %>%
-    group_by(parameter_type) %>% ### median function not behaving in ggplot so going with this even with grouping
-    mutate(median = median(parameter_value,na.rm=TRUE))
+    group_by(parameter_type) %>%
+    mutate(median = median(parameter_value, na.rm = TRUE))
 
   df_plot$article_label_unique <- make.unique(df_plot$article_label)
 
-  df_plot <- df_plot %>% mutate(parameter_type_short =
-                                  ifelse(parameter_type=="Reproduction number (Basic R0)",
-                                         "Basic (R0)",
-                                         ifelse(parameter_type=="Reproduction number (Effective, Re)","Effective (Re)",NA)))
+  df_plot <- df_plot %>%
+    mutate(parameter_type_short =
+             ifelse(parameter_type ==
+                      "Reproduction number (Basic R0)",
+                    "Basic (R0)",
+                    ifelse(parameter_type ==
+                             "Reproduction number (Effective, Re)",
+                           "Effective (Re)", NA)))
 
-  plot <-
-    ggplot(df_plot, aes(x = parameter_value, y = article_label_unique, col = parameter_type_short))+
-    theme_bw()+
-    # scale_x_continuous(breaks = seq(0, 2, by = 0.2))+
-    # geom_errorbar(aes(y=article_label,xmin=parameter_lower_bound,xmax=parameter_upper_bound,
-    #                   group=parameter_data_id,
-    #                   linetype="Parameter range"),
-    #               position=position_dodge(width=0.5),
-    #               width=0.25)+
-    geom_errorbar(aes(y=article_label,
-                      xmin=parameter_uncertainty_lower_value,xmax=parameter_uncertainty_upper_value,
-                      group=parameter_data_id,
-                      linetype="Uncertainty interval"),
-                  position=position_dodge(width=0.5),
+  plot <- ggplot(df_plot, aes(x = parameter_value,
+                              y = article_label_unique,
+                              col = parameter_type_short)) +
+    theme_bw() +
+    geom_errorbar(aes(y = article_label,
+                      xmin = parameter_uncertainty_lower_value,
+                      xmax = parameter_uncertainty_upper_value,
+                      group = parameter_data_id,
+                      linetype = "Uncertainty interval"),
+                  position = position_dodge(width = 0.5),
                   width = 0.2,
-                  lwd=1)+
-    geom_point(aes(x=parameter_value,y=article_label,group=parameter_data_id),size = 3)+
-    # geom_vline(aes(xintercept=median,col="Sample median"),linetype="dashed", colour = "grey") +
+                  lwd = 1) +
+    geom_point(aes(x = parameter_value,
+                   y = article_label,
+                   group = parameter_data_id),
+               size = 3) +
     geom_vline(xintercept = 1, linetype = "dashed", colour = "dark grey") +
-    labs(x="Reproduction number",y="",#y="Study",
-         linetype="",colour="")+
-    scale_linetype_manual(values = c("solid"),labels = function(x) str_wrap(x, width = 5))+
-    #scale_colour_discrete(labels = c("Basic R0", "Effective Re")) +
-    scale_colour_manual(values=c("#D95F02","#7570B3"))+
-    theme(#legend.position="bottom",
-      legend.text = element_text(size=12),
-      strip.text = element_text(size=20)) + xlim(c(0,2)) +
-    guides(colour = guide_legend(order=1,ncol=1),
-           linetype = guide_legend(order=2,ncol=1))
+    labs(x = "Reproduction number", y = "", linetype = "", colour = "") +
+    scale_linetype_manual(values = c("solid"),
+                          labels = function(x) str_wrap(x, width = 5)) +
+    scale_colour_manual(values = c("#D95F02", "#7570B3")) +
+    theme(legend.text = element_text(size = 12),
+          strip.text = element_text(size = 20)) +
+    xlim(c(0, 2)) +
+    guides(colour = guide_legend(order = 1, ncol = 1),
+           linetype = guide_legend(order = 2, ncol = 1))
 
   return(plot)
 }
