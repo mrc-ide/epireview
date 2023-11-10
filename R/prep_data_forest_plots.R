28a873a347685b76d43456622c5f681d47c95a78
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2023-11-10 17:36:13 +0000

    More refactoring

--- R/prep_data_forest_plots.R
+++ R/prep_data_forest_plots.R
@@ -26,50 +26,39 @@ short_parameter_type <- functon(x) {
 }
 
 
-##' Combine parameters and articles for use in forest plots
+##' Retrieve pathogen-specific data
 ##'
 ##' @details
-##'
-##' This function will read in the pathogen-specific articles file and
+##' The data extracted in the systematic review has been stored in three
+##' files - one each for articles, parameters, and transmission models.
+##' Data in the three files can be linked using article identifier.
+##' This function will read in the pathogen-specific articles and
 ##' parameters files and join them into a data.frame. The resulting
-##' data set can be used to create a forest plot/
+##' data set can be used to create a forest plot.
 ##'
 ##' @param pathogen name of pathogen. Must be one of the priority pathogens
 ##' exactly as specified in the package. You can get a list of the
 ##' priority pathogens currently included in the package by calling
 ##' \code{priority_pathogens()}.
 ##' @param prepend
-##' @param exclude
-##' @return data.frame with articles information (authors, publication year)
-##' combined with the parameters
+##'
+##' @return a list of length 2. The first element is a data.frame
+##' called "params" with articles information (authors, publication year)
+##' combined with the parameters. The second element is a data.frame
+##' called "models" with all transmission models extracted for this
+##' pathogen.
 ##' @importFrom readr read_csv
 ##' @importFrom dplyr left_join
 ##' @export
 
-prep_data_forest_plot <- function(pathogen, prepend = "") {
+fetch_data <- function(pathogen, prepend = "") {
   assert_pathogen(pathogen)
   pps <- priority_pathogens()
 
+
+  # Get file path for article data
   afname <- pps[pps$pathogen == pathogen, "articles_file"]
-  pfname <- pps[pps$pathogen == pathogen, "params_file"]
-  mfname <- pps[pps$pathogen == pathogen, "models_file"]
-
-  # Get file pathway for parameter data
-  file_path_pa <- system.file(
-    "extdata", pfname,
-    package = "epireview"
-  )
-  if (file_path_pa == "") {
-    file_path_pa <- paste0(prepend, "inst/extdata/", pathogen, "_parameter.csv")
-  }
-
-  params <- read_csv(file_path_pa, show_col_types = FALSE)
-  params <- short_parameter_type(params)
-
-  # Get file pathway for article data
-  file_path_ar <- system.file(
-    "extdata", afname, package = "epireview"
-  )
+  file_path_ar <- system.file("extdata", afname, package = "epireview")
 
   if (file_path_ar == "") {
     file_path_ar <- paste0(prepend, "inst/extdata/", pathogen, "_article.csv")
@@ -88,13 +77,30 @@ prep_data_forest_plot <- function(pathogen, prepend = "") {
 
   articles <- articles[, cols]
 
-  df <- left_join(params, articles, by = "article_id")
+  # Get file path for parameter data
+  pfname <- pps[pps$pathogen == pathogen, "params_file"]
+  file_path_pa <- system.file("extdata", pfname, package = "epireview")
+  if (file_path_pa == "") {
+    file_path_pa <- paste0(prepend, "inst/extdata/", pathogen, "_parameter.csv")
+  }
 
+  params <- read_csv(file_path_pa, show_col_types = FALSE)
   ## Marburg parameters have entries like "Germany;Yugoslavia"
   ## For future pathogens, this should be cleaned up before data are
   ## checked into epireview
-  df$parameter_value <- as.numeric(df$parameter_value)
+  params <- short_parameter_type(params)
 
-  df
+  ## Get file path for models
+  mfname <- pps[pps$pathogen == pathogen, "models_file"]
+  models <- read_csv(mfname, show_col_types = FALSE)
+
+
+  df1 <- left_join(params, articles, by = "article_id")
+  df1$parameter_value <- as.numeric(df1$parameter_value)
+
+  df2 <- left_join(models, articles, by = "article_id")
+
+
+  list(params = df1, models = df2)
 
 }
