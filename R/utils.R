2f18cef8de0d3956e0fa4d4f7bda8e9864cac893
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2023-11-10 16:30:05 +0000

    Consistent shape values

--- R/utils.R
+++ R/utils.R
@@ -6,10 +6,10 @@
 #' @importFrom ggplot2 theme
 #' @export
 theme_epireview <- function(
-  base_size = 11,
-  base_family = "",
-  base_line_size = base_size/22,
-  base_rect_size = base_size/22) {
+    base_size = 11,
+    base_family = "",
+    base_line_size = base_size / 22,
+    base_rect_size = base_size / 22) {
 
   update_geom_defaults("point", list(size = 3))
   update_geom_defaults("segment", list(lwd = 5, alpha = 0.4))
@@ -40,10 +40,36 @@ parameter_palette <- function(x) {
   out <- list(
     ## Variations of R0
     "Basic (R0)" = "#D95F02",
-    "Reproduction number (Basic R0)"= "#D95F02",
+    "Reproduction number (Basic R0)" = "#D95F02",
     ## Variations of Re
     "Effective (Re)" = "#7570B3",
     "Reproduction number (Effective, Re)" = "#7570B3"
+  )
+
+  out[x]
+}
+##' Define a consistent shape palette for use in
+##'
+##' We map shape aesthetic to value type i.e., mean, median etc.
+##' This function defines a shape palette that can be used in forest
+##' plots
+##' @param x a list of parameters
+##' @return a named list of shapes where names are value types (mean,
+##' median, std dev etc.)
+##'
+##' @author Sangeeta Bhatia
+value_type_palette <- function(x) {
+  out <- list(
+    Mean = 16,
+    mean = 16,
+    average = 16,
+    Median = 15,
+    median = 15,
+    `Std Dev` = 17,
+    `std dev` = 17,
+    sd = 17,
+    other = 18,
+    Other = 18
   )
 
   out[x]
