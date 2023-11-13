6abed0839971d4998730d1dbb1a9ba8cfda5ebf2
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2023-11-13 23:33:54 +0000

    Some tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,4 +1,4 @@
-test_that("filter_cols fails when wrong functions are supplied", {
+test_that("filter_cols fails when wrong arguments are supplied", {
 
   ## Fails if columns are character and function
   ## is numeric
@@ -7,7 +7,36 @@ test_that("filter_cols fails when wrong functions are supplied", {
   expect_error(filter_cols(p, "parameter_type", ">", 21))
   expect_error(filter_cols(p, "parameter_type", "<", 21))
 
-  ## This should work
-  ## filter_cols(p, "parameter_value", "%in%", list(pval = c(20, 21)))
-  ## filter_cols(p, "parameter_value", ">", list(pval = c(20, 21)))
+  ## Non-existent column
+  expect_error(filter_cols(p, "parameter_xyz", "<", 21))
 })
+
+test_that("filter_cols works as expected", {
+
+  ## Fails if columns are character and function
+  ## is numeric
+  x <- fetch_data('marburg')
+  p <- x$params
+  ## Test column of each type: numeric, factor, and character
+  out <- filter_cols(p, "parameter_value", "%in%", list(pval = c(20, 21)))
+  expect_in(unique(out$parameter_value), c(20, 21))
+
+  out <- filter_cols(p, "parameter_value", ">", 20)
+  vals <- all(out$parameter_value > 20, na.rm = TRUE)
+  expect_true(vals)
+
+  out <- filter_cols(
+    p, "population_country", "%in%", list(pc = c("South Africa", "Germany"))
+  )
+  vals <- unique(out$population_country)
+  expect_in(vals, c("South Africa", "Germany"))
+
+  out <- filter_cols(p, "population_country", "==", "xyz")
+  ## Fails because NA is matched.
+  ## TODO check why we have rows with all NAs and whether they need
+  ## to be removed.
+  expect_equal(nrow(out), 0)
+
+})
+
+
