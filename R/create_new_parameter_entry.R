e809658aeb48aa5fa7deb022035c3be0f3b75af6
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2024-01-18 13:02:07 +0000

    Define column types as otherwise NAs are read as logical

--- R/create_new_parameter_entry.R
+++ R/create_new_parameter_entry.R
@@ -14,93 +14,128 @@
 #' create_new_parameter_entry <-
 #'   function(pathogen = "marburg",
 #'            new_param =
-#'              c(list("article_id"            = as.integer(1)),
-#'                list("parameter_type"        = as.character(NA)),
-#'                list("parameter_value"       = as.double(NA)),
-#'                list("parameter_unit"        = as.character(NA)),
-#'                list("parameter_lower_bound" = as.double(NA)),
-#'                list("parameter_upper_bound" = as.double(NA)),
-#'                list("population_study_start_day"   = as.integer(NA)),
-#'                list("population_study_start_month" = as.character(NA)),
-#'                list("population_study_start_year"  = as.integer(NA)),
-#'                list("population_study_end_day"     = as.integer(NA)),
-#'                list("population_study_end_month"   = as.character(NA)),
-#'                list("population_study_end_year"    = as.integer(NA)),
-#'                list("genome_site"                  = as.character(NA)),
-#'                list("genomic_sequence_available"   = as.logical(NA)),
-#'                list("parameter_class"          = as.character(NA)),
-#'                list("covidence_id"             = as.integer(2059)))
+#'              c("article_id"            = as.integer(NA),
+#'                "parameter_type"        = as.character(NA),
+#'                "parameter_value"       = as.double(NA),
+#'                "parameter_unit"        = as.character(NA),
+#'                "parameter_lower_bound" = as.double(NA),
+#'                "parameter_upper_bound" = as.double(NA),
+#'                "parameter_value_type"  = as.character(NA),
+#'                "parameter_uncertainty_single_value" = as.double(NA),
+#'                "parameter_uncertainty_singe_type"   = as.character(NA),
+#'                "parameter_uncertainty_lower_value"  = as.double(NA),
+#'                "parameter_uncertainty_upper_value"  = as.double(NA),
+#'                "parameter_uncertainty_type"         = as.character(NA),
+#'                "cfr_ifr_numerator"       = as.integer(NA),
+#'                "cfr_ifr_denominator"     = as.integer(NA),
+#'                "distribution_type"       = as.character(NA),
+#'                "distribution_par1_value" = as.double(NA),
+#'                "distribution_par1_type"  = as.character(NA),
+#'                "distribution_par1_uncertainty" = as.logical(NA),
+#'                "distribution_par2_value" = as.double(NA),
+#'                "distribution_par2_type"  = as.character(NA),
+#'                "distribution_par2_uncertainty" = as.logical(NA),
+#'                "method_from_supplement"  = as.logical(NA),
+#'                "method_moment_value"     = as.character(NA),
+#'                "cfr_ifr_method"          = as.character(NA),
+#'                "method_r"                = as.character(NA),
+#'                "method_disaggregated_by" = as.character(NA),
+#'                "method_disaggregated"    = as.logical(NA),
+#'                "method_disaggregated_only" = as.logical(NA),
+#'                "riskfactor_outcome"      = as.character(NA),
+#'                "riskfactor_name"         = as.character(NA),
+#'                "riskfactor_occupation"   = as.character(NA),
+#'                "riskfactor_significant"  = as.character(NA),
+#'                "riskfactor_adjusted"     = as.character(NA),
+#'                "population_sex"          = as.character(NA),
+#'                "population_sample_type"  = as.character(NA),
+#'                "population_group"        = as.character(NA),
+#'                "population_age_min"      = as.integer(NA),
+#'                "population_age_max"      = as.integer(NA),
+#'                "population_sample_size"  = as.integer(NA),
+#'                "population_country"      = as.character(NA),
+#'                "population_location"     = as.character(NA),
+#'                "population_study_start_day"   = as.integer(NA),
+#'                "population_study_start_month" = as.character(NA),
+#'                "population_study_start_year"  = as.integer(NA),
+#'                "population_study_end_day"     = as.integer(NA),
+#'                "population_study_end_month"   = as.character(NA),
+#'                "population_study_end_year"    = as.integer(NA),
+#'                "genome_site"                  = as.character(NA),
+#'                "genomic_sequence_available"   = as.logical(NA),
+#'                "parameter_class"          = as.character(NA),
+#'                "covidence_id"             = as.integer(NA)))
 #'            )
 #' @export
 create_new_parameter_entry <-
   function(pathogen = NA,
-           new_param =
-             c(list("article_id"            = as.integer(NA)),
-               list("parameter_type"        = as.character(NA)),
-               list("parameter_value"       = as.double(NA)),
-               list("parameter_unit"        = as.character(NA)),
-               list("parameter_lower_bound" = as.double(NA)),
-               list("parameter_upper_bound" = as.double(NA)),
-               list("parameter_value_type"  = as.character(NA)),
-               list("parameter_uncertainty_single_value" = as.double(NA)),
-               list("parameter_uncertainty_singe_type"   = as.character(NA)),
-               list("parameter_uncertainty_lower_value"  = as.double(NA)),
-               list("parameter_uncertainty_upper_value"  = as.double(NA)),
-               list("parameter_uncertainty_type"         = as.character(NA)),
-               list("cfr_ifr_numerator"       = as.integer(NA)),
-               list("cfr_ifr_denominator"     = as.integer(NA)),
-               list("distribution_type"       = as.character(NA)),
-               list("distribution_par1_value" = as.double(NA)),
-               list("distribution_par1_type"  = as.character(NA)),
-               list("distribution_par1_uncertainty" = as.logical(NA)),
-               list("distribution_par2_value" = as.double(NA)),
-               list("distribution_par2_type"  = as.character(NA)),
-               list("distribution_par2_uncertainty" = as.logical(NA)),
-               list("method_from_supplement"  = as.logical(NA)),
-               list("method_moment_value"     = as.character(NA)),
-               list("cfr_ifr_method"          = as.character(NA)),
-               list("method_r"                = as.character(NA)),
-               list("method_disaggregated_by" = as.character(NA)),
-               list("method_disaggregated"    = as.logical(NA)),
-               list("method_disaggregated_only" = as.logical(NA)),
-               list("riskfactor_outcome"      = as.character(NA)),
-               list("riskfactor_name"         = as.character(NA)),
-               list("riskfactor_occupation"   = as.character(NA)),
-               list("riskfactor_significant"  = as.character(NA)),
-               list("riskfactor_adjusted"     = as.character(NA)),
-               list("population_sex"          = as.character(NA)),
-               list("population_sample_type"  = as.character(NA)),
-               list("population_group"        = as.character(NA)),
-               list("population_age_min"      = as.integer(NA)),
-               list("population_age_max"      = as.integer(NA)),
-               list("population_sample_size"  = as.integer(NA)),
-               list("population_country"      = as.character(NA)),
-               list("population_location"     = as.character(NA)),
-               list("population_study_start_day"   = as.integer(NA)),
-               list("population_study_start_month" = as.character(NA)),
-               list("population_study_start_year"  = as.integer(NA)),
-               list("population_study_end_day"     = as.integer(NA)),
-               list("population_study_end_month"   = as.character(NA)),
-               list("population_study_end_year"    = as.integer(NA)),
-               list("genome_site"                  = as.character(NA)),
-               list("genomic_sequence_available"   = as.logical(NA)),
-               list("parameter_class"          = as.character(NA)),
-               list("covidence_id"             = as.integer(NA)))
+           new_param = list(
+             article_id            = as.integer(NA),
+             parameter_type        = as.character(NA),
+             parameter_value       = as.double(NA),
+             parameter_unit        = as.character(NA),
+             parameter_lower_bound = as.double(NA),
+             parameter_upper_bound = as.double(NA),
+             parameter_value_type  = as.character(NA),
+             parameter_uncertainty_single_value = as.double(NA),
+             parameter_uncertainty_singe_type   = as.character(NA),
+             parameter_uncertainty_lower_value  = as.double(NA),
+             parameter_uncertainty_upper_value  = as.double(NA),
+             parameter_uncertainty_type         = as.character(NA),
+             cfr_ifr_numerator       = as.integer(NA),
+             cfr_ifr_denominator     = as.integer(NA),
+             distribution_type       = as.character(NA),
+             distribution_par1_value = as.double(NA),
+             distribution_par1_type  = as.character(NA),
+             distribution_par1_uncertainty = as.logical(NA),
+             distribution_par2_value = as.double(NA),
+             distribution_par2_type  = as.character(NA),
+             distribution_par2_uncertainty = as.logical(NA),
+             method_from_supplement  = as.logical(NA),
+             method_moment_value     = as.character(NA),
+             cfr_ifr_method          = as.character(NA),
+             method_r                = as.character(NA),
+             method_disaggregated_by = as.character(NA),
+             method_disaggregated    = as.logical(NA),
+             method_disaggregated_only = as.logical(NA),
+             riskfactor_outcome      = as.character(NA),
+             riskfactor_name         = as.character(NA),
+             riskfactor_occupation   = as.character(NA),
+             riskfactor_significant  = as.character(NA),
+             riskfactor_adjusted     = as.character(NA),
+             population_sex          = as.character(NA),
+             population_sample_type  = as.character(NA),
+             population_group        = as.character(NA),
+             population_age_min      = as.integer(NA),
+             population_age_max      = as.integer(NA),
+             population_sample_size  = as.integer(NA),
+             population_country      = as.character(NA),
+             population_location     = as.character(NA),
+             population_study_start_day   = as.integer(NA),
+             population_study_start_month = as.character(NA),
+             population_study_start_year  = as.integer(NA),
+             population_study_end_day     = as.integer(NA),
+             population_study_end_month   = as.character(NA),
+             population_study_end_year    = as.integer(NA),
+             genome_site                  = as.character(NA),
+             genomic_sequence_available   = as.logical(NA),
+             parameter_class          = as.character(NA),
+             covidence_id             = as.integer(NA))
            ) {
 
   #read current article data for pathogen
-    articles <- as_tibble(load_epidata_raw(pathogen = pathogen, "article"))
-    old_parameters <- as_tibble(load_epidata_raw(pathogen = pathogen, "parameter"))
+  articles <- as_tibble(load_epidata_raw(pathogen = pathogen, "article"))
+  old_parameters <- as_tibble(load_epidata_raw(pathogen = pathogen, "parameter"))
   new_row <- as_tibble_row(new_param)
 
   # generate the below quanties.
   new_row$parameter_data_id <- max(old_parameters$parameter_data_id) + 1
-  new_row <- new_row %>% select(colnames(old_parameters))
+  new_row <- new_row[ , colnames(old_parameters)]
 
   # Need to check that article_id & covidence_id exist in the articles table.
   if (!(new_row$article_id %in% articles$article_id &&
-       articles[articles$article_id == new_row$article_id, ]$covidence_id ==
-       new_row$covidence_id))
+        articles[articles$article_id == new_row$article_id, ]$covidence_id ==
+        new_row$covidence_id))
     stop("Article_id + Covidence_id pair does not exist in article data")
 
 
@@ -108,7 +143,7 @@ create_new_parameter_entry <-
   file_path_ob  <- system.file("extdata",
                                paste0(pathogen, "_dropdown_parameters.csv"),
                                package = "epireview")
-  
+
   parameter_options <- read_csv(file_path_ob, show_col_types = FALSE)
 
   # Deal with R CMD Check "no visible binding for global variable"
--- R/load_epidata.R
+++ R/load_epidata.R
@@ -45,7 +45,14 @@ load_epidata_raw <- function(pathogen, table = c("article", "parameter",
     outbreak = pps[pps$pathogen == pathogen, "outbreaks_file"],
     model = pps[pps$pathogen == pathogen, "models_file"],
   )
-  # Get file path for article data
+  ## Get column types based on table type
+  col_types <- switch(
+    table,
+    article = article_column_type(),
+    parameter = parameter_column_type(),
+    outbreak = outbreak_column_type(),
+    model = model_column_type()
+  )
 
   file_path <- system.file("extdata", fname, package = "epireview")
 
@@ -53,9 +60,66 @@ load_epidata_raw <- function(pathogen, table = c("article", "parameter",
     warning(paste("No data found for ", pathogen))
   } else {
     message(paste("Loading data for ", pathogen))
-    out <- read_csv(file_path, show_col_types = FALSE)
+    out <- read_csv(file_path, col_types = col_types, show_col_types = FALSE)
   }
 
   out
 
+}
+
+
+## Define column types for each table
+## These are used to read in the data
+## and to validate the data
+#' Define the column types for the article data frame
+#'
+#' This function defines the column types for the article data frame used in the epireview package.
+#' It specifies the data types for each column in the data frame. readr is generally good at guessing the
+#' column types, but it is better to be explicit. Moreover, it reads a column of NAs as a logical vector, which 
+#' is particularly undesirable for us. 
+#' The function is intended to be used
+#' internally by \code{load_epidata_raw} where the files are being read. 
+#'
+#' @return A list of column types for the article data frame
+#' @importFrom readr col_character col_integer col_logical
+#' @export
+article_column_type <- function() {
+  list(
+    first_author_first_name = col_character(),
+    first_author_surname = col_character(),
+    article_title = col_character(),
+    doi = col_character(),
+    journal = col_character(),
+    year_publication = col_integer(),
+    volume = col_integer(),
+    issue = col_integer(),
+    page_first = col_integer(),
+    page_last = col_integer(),
+    paper_copy_only = col_logical(),
+    notes = col_character(),
+    qa_m1 = col_integer(),
+    qa_m2 = col_integer(),
+    qa_a3 = col_integer(),
+    qa_a4 = col_integer(),
+    qa_d5 = col_integer(),
+    qa_d6 = col_integer(),
+    qa_d7 = col_integer()
+  )
+}
+
+parameter_column_type <- function() {
+  list(
+    article_id = col_integer(),
+    parameter_name = col_character(),
+    parameter_value = col_character(),
+    parameter_units = col_character(),
+    parameter_notes = col_character(),
+    qa_m1 = col_integer(),
+    qa_m2 = col_integer(),
+    qa_a3 = col_integer(),
+    qa_a4 = col_integer(),
+    qa_d5 = col_integer(),
+    qa_d6 = col_integer(),
+    qa_d7 = col_integer()
+  )
 }
