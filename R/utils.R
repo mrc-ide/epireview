7cb4a5df81e01c1ddba87202fdf79c4347f68180
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2024-01-22 18:23:29 +0000

    Remove ugly sytax

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,6 @@
 #' Plotting theme for epireview
 #' A standard theme for figures in epireview.
 #' @inheritParams ggplot2::theme_bw
-#' @importFrom ggplot2 update_geom_defaults
-#' @importFrom ggplot2 theme_bw
-#' @importFrom ggplot2 theme unit element_text margin
 #' @export
 theme_epireview <- function(
     base_size = 11,
@@ -11,15 +8,15 @@ theme_epireview <- function(
     base_line_size = base_size / 22,
     base_rect_size = base_size / 22) {
 
-  ggplot2::update_geom_defaults("point", list(size = 3))
-  ggplot2::update_geom_defaults("segment", list(lwd = 5, alpha = 0.4))
-  ggplot2::update_geom_defaults("errorbar", list(lwd = 1, width = 0.4))
-  th <- ggplot2::theme_bw(base_size, base_family, base_line_size, base_rect_size)
-  th <- th + ggplot2::theme(
-    plot.title = ggplot2::element_text(hjust = 0.5),
-    plot.subtitle = ggplot2::element_text(hjust = 0.5),
-    plot.margin = ggplot2::margin(10, 20, 10, 20),
-    panel.spacing = ggplot2::unit(1.5, "lines"),
+  update_geom_defaults("point", list(size = 3))
+  update_geom_defaults("segment", list(lwd = 5, alpha = 0.4))
+  update_geom_defaults("errorbar", list(lwd = 1, width = 0.4))
+  th <- theme_bw(base_size, base_family, base_line_size, base_rect_size)
+  th <- th + theme(
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    plot.margin = margin(10, 20, 10, 20),
+    panel.spacing = unit(1.5, "lines"),
     legend.position = "bottom"
   )
 
