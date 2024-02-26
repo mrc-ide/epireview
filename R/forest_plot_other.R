6d3e87d7c4d857c6611b0bf4913181b93b927619
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2024-02-26 16:06:14 +0000

    Lots more plots and documentation

---
+++ R/forest_plot_other.R
@@ -0,0 +1,21 @@
+#' Calculate and plot the forest plot for doubling time
+#'
+#' This function calculates the doubling time and creates a forest plot
+#' to visualize the results.
+#' @inheritParams forest_plot_delay_int
+#'
+#' @return A forest plot object.
+#'
+#' @examples
+#' df <- data.frame(parameter_type = c("Doubling time", "Doubling time"),
+#'                  value = c(5, 10))
+#' forest_plot_doubling_time(df, ulim = 20, reorder_studies = TRUE)
+#'
+#' @export
+forest_plot_doubling_time <- function(df, ulim, reorder_studies, ...) {
+    check_ulim(df, ulim, "doubling time")
+    x <- df[df$parameter_type %in% c("Doubling time"), ]
+    p <- forest_plot_delay_int(x, ulim, reorder_studies, ...) +
+        labs(x = "Doubling time (days)")
+    p
+}
