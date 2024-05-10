748afa7ec7d232880a282658e1ad9d4c91cb98f7
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2024-05-10 13:39:26 +0100

    Suppress warnings and messages. Move repeated code at the top

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,9 +1,10 @@
+x <- suppressWarnings(suppressMessages(load_epidata('marburg')))
+p <- x$params
 test_that("filter_cols fails when wrong arguments are supplied", {
 
   ## Fails if columns are character and function
   ## is numeric
-  x <- load_epidata('marburg')
-  p <- x$params
+
   expect_error(filter_cols(p, "parameter_type", ">", 21))
   expect_error(filter_cols(p, "parameter_type", "<", 21))
 
@@ -15,8 +16,6 @@ test_that("filter_cols works as expected", {
 
   ## Fails if columns are character and function
   ## is numeric
-  x <- load_epidata('marburg')
-  p <- x$params
   ## Test column of each type: numeric, factor, and character
   out <- filter_cols(p, "parameter_value", "in", list(pval = c(20, 21)))
   expect_in(unique(out$parameter_value), c(20, 21))
@@ -32,9 +31,6 @@ test_that("filter_cols works as expected", {
   expect_in(vals, c("South Africa", "Germany"))
 
   out <- filter_cols(p, "population_country", "==", "xyz")
-  ## Fails because NA is matched.
-  ## TODO check why we have rows with all NAs and whether they need
-  ## to be removed.
   expect_equal(nrow(out), 0)
 
 })
