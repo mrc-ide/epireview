312669b8051c42c2e7f4af65410ff7e64d0d6440
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2024-05-10 14:30:15 +0100

    Update test as load_epidata now also returns articles

--- tests/testthat/test_data_load_and_prep.R
+++ tests/testthat/test_data_load_and_prep.R
@@ -1,8 +1,8 @@
 test_that("load_epidata loads data as expected", {
 
-  out <- load_epidata("marburg")
-  expect_equal(length(out), 3)
-  expect_equal(names(out), c("params", "models", "outbreaks"))
+  out <- suppressWarnings(suppressMessages(load_epidata("marburg")))
+  expect_equal(length(out), 4)
+  expect_equal(names(out), c("articles", "params", "models", "outbreaks"))
   expect_equal(nrow(out$params), 70)
   expect_equal(nrow(out$models), 1)
   expect_equal(nrow(out$outbreaks), 23)
