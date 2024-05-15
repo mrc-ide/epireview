7b9ccb348e39e65342bb7af1296f3af4bc9c9f3b
Thom Rawson <t.rawson@imperial.ac.uk>
2024-05-15 16:05:45 +0100

    Switch off unique y-axis labels.

--- R/forest_plot.R
+++ R/forest_plot.R
@@ -23,7 +23,7 @@
 #' If you wish to color by a different variable, you must provide a palette.
 #' @return A ggplot2 object representing the forest plot.
 #' @import ggplot2
-#' @export 
+#' @export
 #' @examples
 #' df <- data.frame(
 #'   mid = c(1, 2, 3),
@@ -34,9 +34,9 @@
 #' )
 #' forest_plot(df)
 forest_plot <- function(df, facet_by = NA, shape_by = NA, col_by = NA,
-    shp_palette = NA, 
+    shp_palette = NA,
     col_palette = NA) {
- 
+
   ## ggplot2 will put all article labels on the y-axis
   ## even if mid, low, and high are NA. We will filter them out
   ## here to avoid that.
@@ -46,10 +46,10 @@ forest_plot <- function(df, facet_by = NA, shape_by = NA, col_by = NA,
     any(!is.na(x[c("mid", "low", "high")]))
   }, simplify = TRUE)
   df <- df[rows, ]
-  ## We don't want to plot rows where mid_type is "Range midpoint" or 
+  ## We don't want to plot rows where mid_type is "Range midpoint" or
   ## "Uncertainty width".
   df$mid[df$mid_type %in% c("Range midpoint")] <- NA
-    
+
   ## uncertainty_type was created by us in param_pm_uncertainty
   ## so the user has no visibility of this variable. The main thing
   ## is that we want to distinguish Range** which is slightly different
@@ -61,22 +61,27 @@ forest_plot <- function(df, facet_by = NA, shape_by = NA, col_by = NA,
   ## note that if you use dashed linetype here, then the legend only shows
   ## a single dash, which is of course indisguishable from a solid line.
 
+  # Preprocess the dataframe to remove bracketed numbers
+  df$article_label_clean <- gsub("\\s*\\(\\d+\\)$", "", df$article_label)
+
   p <- ggplot(df) +
-    geom_point(aes(x = .data[['mid']], y = .data[['article_label']])) +
+    geom_point(aes(x = .data[['mid']],
+                   y = .data[['article_label']])) +
     geom_errorbar(
       aes(xmin = .data[['low']], xmax = .data[['high']], y = .data[['article_label']],
           lty = .data[['uncertainty_type']])
     ) +
     scale_linetype_manual(values = lty_map, breaks = "Range**") +
-    ##scale_y_discrete(breaks = df$article_label, labels = df$article_label) + 
+    scale_y_discrete(labels = df$article_label_clean) +
+    ##scale_y_discrete(breaks = df$article_label, labels = df$article_label) +
     theme_epireview()
-  
+
   p <- p + theme(axis.title.y = element_blank())
 
   if (!is.na(facet_by)) {
     p <- p + facet_col(
       ~.data[[facet_by]], scales = "free_y", space = "free"
-    ) 
+    )
   }
 
   if (!is.na(shape_by)) {
@@ -92,12 +97,12 @@ forest_plot <- function(df, facet_by = NA, shape_by = NA, col_by = NA,
         p <- p + scale_shape_manual(values = shp_palette)
       } else {
         ## if no palette is found, use the default and issue a warning
-        warning(paste("No palette was provided or found for ", shape_by, ". 
+        warning(paste("No palette was provided or found for ", shape_by, ".
           Using default palette"))
       }
-     
-      
-    }   
+
+
+    }
   }
 
   if (!is.na(col_by)) {
@@ -113,10 +118,10 @@ forest_plot <- function(df, facet_by = NA, shape_by = NA, col_by = NA,
         p <- p + scale_color_manual(values = col_palette)
       } else {
         ## if the palette is not found, use the default and issue a warning
-        warning(paste("No palette was provided or found for ", col_by, ". 
+        warning(paste("No palette was provided or found for ", col_by, ".
         Using default palette"))
       }
-      
+
     }
   }
   p
