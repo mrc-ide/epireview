a117f4e8f857d69c097ac3a72bf36540d3d83ecd
Kelly McCain <k.mccain22@imperial.ac.uk>
2024-06-06 15:47:25 +0100

    add manual fixing of other delays for Marburg and Lassa

--- R/prep_data_forest_plots.R
+++ R/prep_data_forest_plots.R
@@ -4,15 +4,15 @@
 ##' names. It is generally not intended to be called directly but is used by
 ##' \code{\link{load_epidata}} when the data is loaded.
 ##'
-##' @param x data.frame containing a column called "parameter_type",  This will 
+##' @param x data.frame containing a column called "parameter_type",  This will
 ##' typically be the `params`data.frame from the output of \code{load_epireview}.
-##' @param parameter_type_full optional. User can specify the full value of a 
+##' @param parameter_type_full optional. User can specify the full value of a
 ##' parameter type not already included in the function.
 ##' @param parameter_type_short optional. Shorter value of parameter_type_full
 ##' @return data.frame with a new column called "parameter_type_short"
 ##' @export
 ##' @author Sangeeta Bhatia
-short_parameter_type <- function(x, parameter_type_full, parameter_type_short) {
+short_parameter_type <- function(x, pathogen, parameter_type_full, parameter_type_short) {
 
   x$parameter_type_short <- case_when(
     x$parameter_type == "Reproduction number (Basic R0)" ~ "Basic (R0)",
@@ -22,6 +22,18 @@ short_parameter_type <- function(x, parameter_type_full, parameter_type_short) {
     x$parameter_type == "Human delay - time symptom to outcome" &
       x$riskfactor_outcome == "Other" ~ "Time symptom to outcome (Other)"
   )
+
+  if(pathogen == 'marburg'){
+    x$parameter_type <- ifelse(x$parameter_type == 'Human delay - time symptom to outcome',
+                               paste0('Human delay - ', tolower(x$parameter_type_short)), x$parameter_type)
+  } else if(pathogen == 'lassa'){
+    x$other_delay <- ifelse(x$parameter_type == 'Human delay - other human delay (go to section)',
+                            paste0(tolower(str_replace(x$other_delay_start, 'Other: ', '')), ' to ',
+                                   tolower(str_replace(x$other_delay_end, 'Other: ', ''))), NA)
+
+    x$parameter_type <- ifelse(x$parameter_type == 'Human delay - other human delay (go to section)',
+                               paste0('Human delay - ', x$other_delay), x$parameter_type)
+  }
 
   if (! missing(parameter_type_full) & ! missing(parameter_type_short)) {
     x$parameter_type_short <- case_when(
@@ -50,7 +62,7 @@ short_parameter_type <- function(x, parameter_type_full, parameter_type_short) {
 #' exactly as specified in the package. You can get a list of the
 #' priority pathogens currently included in the package by calling
 #' @seealso \code{\link{load_epidata}}
-#' 
+#'
 #' @param mark_multiple logical. If TRUE, multiple studies from the same
 #' author in the same year will be marked with a suffix to distinguish them.
 #' @return a list of length 2. The first element is a data.frame
@@ -88,12 +100,12 @@ load_epidata <- function(pathogen, mark_multiple = TRUE) {
     warning(paste("No outbreaks information found for ", pathogen))
     outbreaks_extracted <- FALSE
   }
-  
+
   if (! inherits(params, "data.frame")) {
     warning(paste("No params information found for ", pathogen))
     params_extracted <- FALSE
   }
-  
+
 
   articles <- pretty_article_label(articles, mark_multiple)
   cols <- c(
@@ -105,14 +117,14 @@ load_epidata <- function(pathogen, mark_multiple = TRUE) {
   ## Marburg parameters have entries like "Germany;Yugoslavia"
   ## For future pathogens, this should be cleaned up before data are
   ## checked into epireview
-  params <- short_parameter_type(params)
+  params <- short_parameter_type(params, pathogen)
   params$parameter_value <- as.numeric(params$parameter_value)
-  
+
   if (params_extracted) {
     params <- left_join(params, articles, by = "id") |>
       mark_multiple_estimates("parameter_type", label_type = "numbers")
   } else params <- NULL
-  
+
   if (models_extracted) {
     models <- left_join(models, articles, by = "id") |>
       mark_multiple_estimates("model_type", label_type = "numbers")
@@ -122,34 +134,34 @@ load_epidata <- function(pathogen, mark_multiple = TRUE) {
     outbreaks <- left_join(outbreaks, articles, by = "id") |>
       mark_multiple_estimates("outbreak_country", label_type = "numbers")
   } else outbreaks <- NULL
-  
+
   message("Data loaded for ", pathogen)
-  
+
   list(
-    articles = articles_everything, params = params, models = models, 
+    articles = articles_everything, params = params, models = models,
     outbreaks = outbreaks
   )
 }
 
 
 #' Distinguish multiple estimates from the same study
-#' 
-#' @details 
+#'
+#' @details
 #' If a study has more than one estimate for the same parameter_type/model/outbreak,
 #' we add a suffix to the article_label to distinguish them
 #' otherwise they will be plotted on the same line in the forest plot. Say
 #' we have two estimates for the same parameter_type (p) from the same study (s),
 #' they will then be labeled as s 1 and s 2.
-#' 
-#' 
+#'
+#'
 #'
 #' @param df The data frame containing the estimates.
 #' @param col The column name for the table type. For parameters this is
-#' "parameter_type"; for models this is "model_type"; for outbreaks this is 
+#' "parameter_type"; for models this is "model_type"; for outbreaks this is
 #' "outbreak_country".
 #'
-#' @param label_type Type of labels to add to distinguish multiple estimates. 
-#' Must be one of "letters" or "numbers". 
+#' @param label_type Type of labels to add to distinguish multiple estimates.
+#' Must be one of "letters" or "numbers".
 #' @return The modified data frame with updated article_label
 #'
 #' @examples
