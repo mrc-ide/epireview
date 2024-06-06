2b320aa0b673d81a374e4359b638f4c5d53e69fb
patcatgit <patrick.doohan15@imperial.ac.uk>
2024-06-06 17:34:12 +0100

    Changed population min and max column types to doubles, since non-integer values are reported in certain articles. Changed asymptomatic_transmission reported to logical, consistent with database

--- R/load_epidata.R
+++ R/load_epidata.R
@@ -15,7 +15,7 @@
 #' "article", "parameter", "outbreak", or "model"
 #'
 #'
-#' 
+#'
 #' @return data.frame reading in the csv the specified pathogen table
 #' @importFrom readr read_csv
 #' @seealso
@@ -25,19 +25,19 @@
 #' @export
 load_epidata_raw <- function(pathogen, table = c("article", "parameter",
                                                 "outbreak", "model")) {
-  
+
   # assertions
-  
+
   if (missing(pathogen) | missing(table)) {
     stop("pathogen and table name must be supplied. table can be
          one of 'article', 'parameter', 'outbreak' or 'model'")
   }
-  
+
   assert_pathogen(pathogen)
   assert_table(table)
-  
+
   pps <- priority_pathogens()
-  
+
   fname <- switch(
     table,
     article =  pps[pps$pathogen == pathogen, "articles_file"],
@@ -53,13 +53,13 @@ load_epidata_raw <- function(pathogen, table = c("article", "parameter",
     outbreak = outbreak_column_type(),
     model = model_column_type()
   )
-  
+
   if (is.na(fname)) {
     warning(paste("No data found for ", pathogen))
     return(NULL)
   } else {
     file_path <- system.file("extdata", fname, package = "epireview")
-    ## Temporarily read in without column types as column names for the 
+    ## Temporarily read in without column types as column names for the
     ## same table can change between pathogens
     tmp <- read_csv(file_path, show_col_types = FALSE)
     ## col_types will have more columns than tmp, so we need to select
@@ -69,10 +69,10 @@ load_epidata_raw <- function(pathogen, table = c("article", "parameter",
     ## parameters.
     cols <- intersect(colnames(tmp), names(col_types))
     col_types <- col_types[cols]
-    out <- read_csv(file_path, col_types = col_types, show_col_types = FALSE, col_select = colnames(tmp)) 
+    out <- read_csv(file_path, col_types = col_types, show_col_types = FALSE, col_select = colnames(tmp))
   }
   out
-  
+
 }
 
 
@@ -83,10 +83,10 @@ load_epidata_raw <- function(pathogen, table = c("article", "parameter",
 #'
 #' This function defines the column types for the article data frame used in the epireview package.
 #' readr is generally good at guessing the
-#' column types, but it is better to be explicit. Moreover, it reads a column of NAs as a logical vector, which 
-#' is particularly undesirable for us. 
+#' column types, but it is better to be explicit. Moreover, it reads a column of NAs as a logical vector, which
+#' is particularly undesirable for us.
 #' The function is intended to be used
-#' internally by \code{load_epidata_raw} where the files are being read. 
+#' internally by \code{load_epidata_raw} where the files are being read.
 #' @inheritParams load_epidata_raw
 #' @return A list of column types for the article data frame
 #' @importFrom readr col_character col_integer col_logical
@@ -125,8 +125,8 @@ article_column_type <- function(pathogen) {
 #'
 #' This function defines the column types for the parameters in the dataset.
 #' It returns a list of column types with their corresponding names.
-#' 
-#' @inherit article_column_type details return seealso 
+#'
+#' @inherit article_column_type details return seealso
 #' @export
 #'
 #' @examples
@@ -175,8 +175,8 @@ parameter_column_type <- function() {
     population_sex = col_character(),
     population_sample_type = col_character(),
     population_group = col_character(),
-    population_age_min = col_integer(),
-    population_age_max = col_integer(),
+    population_age_min = col_double(),
+    population_age_max = col_double(),
     population_sample_size = col_integer(),
     population_country = col_character(),
     population_location = col_character(),
@@ -191,16 +191,16 @@ parameter_column_type <- function() {
     parameter_class = col_character(),
     covidence_id = col_integer()
   )
-  
+
   out
 }
 
 
 #' outbreak_column_type
-#' 
+#'
 #' This function defines the column types for the outbreaks in the dataset.
 #' It returns a list of column types with their corresponding names.
-#' 
+#'
 #' @inherit article_column_type details return seealso
 #' @export
 #' @importFrom readr col_integer col_character col_double col_logical
@@ -220,7 +220,7 @@ outbreak_column_type <- function() {
     outbreak_date_year   = col_double(),
     outbreak_duration_months = col_double(),
     outbreak_size        = col_double(),
-    asymptomatic_transmission = col_double(),
+    asymptomatic_transmission = col_logical(),
     outbreak_country     = col_character(),
     outbreak_location    = col_character(),
     cases_confirmed      = col_double(),
@@ -235,18 +235,18 @@ outbreak_column_type <- function() {
 }
 
 #' model_column_type
-#' 
+#'
 #' This function defines the column types for the models in the dataset.
 #' It returns a list of column types with their corresponding names.
 #' @inherit article_column_type details return seealso
-#' 
+#'
 #' @export
 #' @importFrom readr col_integer col_character col_double col_logical
 #' @keywords dataset, column types
 #' @examples
 #' model_column_type()
 model_column_type <- function() {
-  
+
   out <- list(
     id = col_character(),
     model_data_id       = col_character(),
